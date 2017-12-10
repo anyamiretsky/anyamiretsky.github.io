6de37656e67da21703915163d5d01e6e57278395
anyamiretsky <33642883+anyamiretsky@users.noreply.github.com>
2017-12-10 12:41:28 -0500

    Miretsky Classwork Class 6-7

---
+++ Class-work/Class7/first-javascript/script.js
@@ -0,0 +1,21 @@
+var mult =10 * 10;
+var div = 100 / 10;
+var difference =0 - 17;
+var sum = 50 + 17;
+console.log(mult);
+var first='Anya';
+var last='Miretsky';
+console.log(`My name is ${first} ${last} `);
+
+function onra() {
+    var city="Saigon";
+    console.log(`Last Tango In ${city}`);
+
+
+}
+onra();
+onra();
+onra();
+
+var hello = document.getElementById('hello');
+hello.style.color ="tomato";
