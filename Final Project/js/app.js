3a19600084dfa26015b888ead2359d5b26c02f24
anyamiretsky <33642883+anyamiretsky@users.noreply.github.com>
2018-01-22 08:08:34 -0500

    Miretsky- Final Project - Cleaned up a little

--- Final Project/js/app.js
+++ Final Project/js/app.js
@@ -32,81 +32,92 @@ function lookupTaskPoints(task){
 
 
 $(document).ready(function(){
-    //fill in the weekOf label
     
 
-    //check if end of the game (last day of the week or later
+    //User
+        //set focus to the correct user tab
+        //show data for that user
+
+    //Winner
+        //check if end of the game (last day of the week or later
         // and calculate winner 
 
     //populate days - get seven days starting on start day
         //re-populate object for the week's game
         //object needs list of days and players with corresponding points
 
-    $('.btnSetDate').click(function(){
-        let startDate = $('.newStartDate').val();
-        //if value of date not null then
-        if(!Date.parse(startDate)){
-            alert('Please entry a valid date');
-        }
-        else{
-            
-
-            //create an object with seven days and fill it with the values starting with start date
-            var gameDates = [];
-            var times = 7;
-            for(var i=0; i < times; i++){
-                let day = moment(startDate).add(i, 'days').format("dddd MMM Do YYYY"); 
-                gameDates.push(day);
-                // console.log('Day'+i+':' + day);
-                let d=i+1;
-                $('.day'+ d).html(day);
-            }
-            
-            let weekStart=moment(startDate).format("dddd MMM Do YYYY");         
-            $('.weekOf').html('Current game ends on ' + gameDates[6]);
-        }
-    })
 
     //switch players by clickin on tabs - load new form
+        //show hide data?
+
+    //click a checkbox - trigger Firebase change event
 
 
-    //close all days to start with
+    //hide all day details to start with
     $('.wrapper').slideUp();
     //show/hide days
     $('.dayHead').click(function(){
         $(this).parent().find('.wrapper').slideToggle();
     })
 
-
-
-    //fill up the task dropdown
+    //fill up the task dropdowns
     for (var i=0;i<taskList.length;i++){
         let taskDesc=taskList[i][1]+ '(' + taskList[i][2] + ')';
         $('<option/>').val(taskList[i][0]).html(taskDesc).appendTo('.tasks');
     }
 
 
-    //add to day's list from task selected in the dropdown 
+    //Set new game  
+    $('.btnSetDate').click(function(){
+
+        //get the new start date
+        let startDate = $('.newStartDate').val();
+        if(!Date.parse(startDate)){
+            alert('Please entry a valid date');
+        }
+        else{
+            //create an object with seven days of the game
+            var gameDates = [];
+            var times = 7;
+            for(var i=0; i < times; i++){
+                let day = moment(startDate).add(i, 'days').format("dddd MMM Do YYYY"); 
+                gameDates.push(day);
+                let d=i+1;
+                $('.day'+ d).html(day);
+            }
+            
+            //let weekStart=moment(startDate).format("dddd MMM Do YYYY");         
+            $('.weekOf').html('Game Ends on ' + gameDates[6]);
+        }
+    })
+
+
+
+    //Add a Task
     $('.add').click(function(){
         //get the value of this day's dropdown only
         let task = $(this).parent().children('.tasks').val();
         //add to the current day's list only
         $(this).parent().children('ul').append("<li><input id="+task+" type='checkbox'>"+ lookupTaskDescription(task) + "<a href='javascript:void(0);' class='remove'>&times;</a></li>");
 
-        //add to firebase
+
+
+        //add to Firebase - test write
         writeFirebase();
 
     })
     
     
-    //remove task if X button is clicked
+    //Remove a Task
     $(document).on("click", "a.remove" , function() {
         $(this).parent().remove();
+
+        //read Firebase - test read
         readFirebase();
     });
 
 
-    //click on checkbox adds points to total for player
+    
 
 
 });
